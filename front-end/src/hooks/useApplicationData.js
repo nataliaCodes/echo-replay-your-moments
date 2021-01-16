41d4d2e284f57103219097a87037aa3ad00f9ebb
Natalia Martian <natalia.martian@gmail.com>
2021-01-16 12:52:57 -0500

    Install axios + set up useApplicationData hook

---
+++ front-end/src/hooks/useApplicationData.js
@@ -0,0 +1,41 @@
+import {
+  useEffect,
+  useReducer
+} from 'react';
+
+import dataReducer, {
+  SET_USERS
+} from '../helpers/dataReducer';
+
+import axios from 'axios';
+
+const useApplicationData = () => {
+  const [state, dispatch] = useReducer(dataReducer, {
+      users: [],
+      loading: true,
+  });
+  useEffect(() => {
+      axios({
+              method: 'GET',
+              url: '/api/users',
+          })
+          .then(({
+              data
+          }) => {
+              console.log('axios done');
+              console.log(data);
+              dispatch({
+                  type: SET_USERS,
+                  users: data
+              });
+          })
+          .catch((err) => console.log(err));
+  }, []);
+
+  return {
+      state,
+      dispatch,
+  };
+};
+
+export default useApplicationData;
