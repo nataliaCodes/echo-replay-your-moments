1aa6a35dfb4f49f13b11a8ad0a0f351b5b91faf4
Natalia Martian <natalia.martian@gmail.com>
2021-01-16 23:33:52 -0500

    Set up input validation + user creation

--- back-end/routes/register.js
+++ back-end/routes/register.js
@@ -1,70 +1,45 @@
-var express = require('express');
-var register = express.Router();
+const express = require('express');
+const register = express.Router();
+const bcrypt = require('bcrypt');
 
-module.exports = () => {
-
+module.exports = ({
+  getUserByEmail,
+  addUser
+}) => {
 
   //handles input from register form
   register.post('/', (req, res) => {
+    console.log('[DBG][line:12][file:register] post');
 
-    console.log('[DBG][line:10][file:register] post');
-    const { newUser } = req.body;
-    console.log(newUser);
+    //deconstruct info sent from form
+    const { firstName, lastName, email, password } = req.body.userInput;
+    const hashedPassword = bcrypt.hashSync(password, 12);
+    const avatar = 'https://raw.githubusercontent.com/schenn1992/smart_TODO_list/master/public/avatars/identicon3.png';
+    
+
+    //search DB for user email
+    getUserByEmail(email)
+      .then(user => {
+
+          //if user exists send back message
+          if (user) {
+              res.json({
+                  msg: 'Sorry, a user account with this email already exists'
+              });
+
+          } else {
+              console.log('user created')
+              return addUser(firstName, lastName, email, hashedPassword, avatar);
+          }
+      })
+      //then send the new user back to the browser
+      .then(newUser => res.json(newUser))
+      .catch(err => res.json({
+          error: err.message
+      }));
+
+
 
   })
   return register;
 };
-
-
-// //gets register page
-// register.get('/', (req, res) => {
-//   const templateVars = {
-//     user: users[req.session.user_id]
-//   };
-  
-//   res.render('register', templateVars);
-// });
-
-// register.post('/', (req, res) => {
-//   console.log(req.body);
-// });
-
-// //handles input on register page
-// app.post('/register', (req, res) => {
-//   //storing email and password as plain text
-//   const { email, password } = req.body;
-
-//   const templateVars = {
-//     email,
-//     password,
-//     error: 'Check your spelling and try again!',
-//     error2: '(Input fields cannot be empty)'
-//   };
-
-//   //if any of the two fields are empty, send error
-//   if (!email || !password) {
-//     return res.render('error', templateVars);
-//   }
-  
-//   //if the email address is already in use, send error
-//   if (getUser(email, users)) {
-//     return res.render('error', templateVars);
-//   }
-  
-//   //else, create new user with email and hashed password
-//   const id = generateRandomId(); //<-- generates new user id
-//   const hashedPassword = bcrypt.hashSync(password, 10);
-
-//   const newUser = {
-//     id,
-//     email,
-//     password: hashedPassword
-//   };
-  
-//   //add user to users database
-//   users[newUser.id] = newUser;
-  
-//   //create cookie with user id
-//   req.session.user_id = id;
-//   res.redirect('/urls');
-// });
