44467f4dd57106f4789030347146623e121024d4
Kelvin Tobar <71950186+ktobar@users.noreply.github.com>
2021-01-21 00:01:05 -0500

    add alert for vids with 0 durations and modal on video save

--- front-end/src/components/VideoPlayer.jsx
+++ front-end/src/components/VideoPlayer.jsx
@@ -1,15 +1,23 @@
+import { useState } from 'react';
 import YouTube from 'react-youtube';
+import Alert from 'react-bootstrap/Alert';
 
 
 export default function YTplayer(props) {
-  
+  const [show, setShow] = useState(false);
+
   const videoOnReady=(event) =>{
     // access to player in all event handlers via event.target
     const player = event.target;
     
-    player.setLoop(true)
+    // player.setLoop(true)
     console.log("Video Duration",player.getDuration())
     let vd = player.getDuration()
+    //set alert
+    if (vd <= 0) {
+      setShow(true)
+    };
+
     props.setVideoInfo(prev=>({...prev, duration: vd}))
   }
   const videoOnPlay=(event) =>{
@@ -30,13 +38,35 @@ export default function YTplayer(props) {
     playerVars: {
       // https://developers.google.com/youtube/player_parameters
       autoplay: 1,
+      loop: 1,
       start: props.videoInfo.startTime,
       end: props.videoInfo.endTime
 
     },
   };
-  
-  return ( 
+
+  if (show) {
+    return (
+      <div>
+        <Alert variant="warning" onClose={() => setShow(false)} dismissible>
+          <Alert.Heading>Video is a Live Stream</Alert.Heading>
+          <p>
+            Sorry, Moment actions will not work with Live Broadcast.
+          </p>
+        </Alert>
+        <YouTube 
+          videoId={props.videoInfo.selectedVideoID} 
+          opts={opts} 
+          onReady={videoOnReady}
+          onPlay={videoOnPlay}
+          onEnd={videoOnEnd}
+        />
+      </div>
+    );
+  }
+
+  return (
+
     <YouTube 
       videoId={props.videoInfo.selectedVideoID} 
       opts={opts} 
