39afbc951c67d68fe513a13937ff23d158326292
Natalia Martian <natalia.martian@gmail.com>
2021-01-21 01:46:48 -0500

    Set up DB helpers for add and delete category

--- back-end/helpers/dbHelpers.js
+++ back-end/helpers/dbHelpers.js
@@ -70,6 +70,26 @@ module.exports = (db) => {
     .catch(err => console.log(err));
   };
 
+  const addCategory = (name) => {
+    const query = {
+      text: `INSERT INTO categories name VALUES $1 RETURNING *` ,
+      values: [name]
+    }
+    return db.query(query)
+        .then(result => result.rows[0])
+        .catch(err => err);
+  };
+
+  const deleteCategory = (id) => {
+    const query = {
+      text: `DELETE FROM categories WHERE id=$1 RETURNING *` ,
+      values: [id]
+    }
+    return db.query(query)
+        .then(result => result.rows[0])
+        .catch(err => err);
+  };
+
   return {
       getUsers,
       getUserByEmail,
